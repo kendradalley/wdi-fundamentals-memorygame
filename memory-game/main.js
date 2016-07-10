b8d44659f8707a1b7cce3668faa44431cd2067ba
kendradalley <kendracdalley@gmail.com>
2016-07-09 23:34:47 -0700

    adding game logic to memory game

--- memory-game/main.js
+++ memory-game/main.js
@@ -4,3 +4,24 @@ var cardOne ="queen";
 var cardTwo ="king";
 var cardThree ="king";
 var cardFour ="queen";
+
+/* If cardOne=cardTwo return alert "Sorry you didn't get a match"*/
+/* If cardOne=cardThree return alert "Sorry you didn't get a match"*/
+/* If cardOne=cardFour return alert "You got a match!"*/
+/* If cardTwo=cardThree return alert "You got a match!"*/
+/* If cardTwo=cardFour return alert "Sorry you didn't get a match"*/
+/* If cardThree=cardFour return alert "Sorry you didn't get a match"*/
+
+if (cardOne===cardTwo) {
+	alert ("Sorry you didn't get a match");
+}	else if (cardOne === cardThree) {
+	alert ("Sorry you didn't get a match");
+}	else if (cardOne === cardFour) {
+	alert ("You got a match");
+}	else if (cardTwo === cardThree) {
+	alert ("You got a match");
+}	else if (cardTwo === cardFour) {
+	alert ("Sorry you didn't get a match");
+}	else if (cardThree === cardFour) {
+	alert ("Sorry you didn't get a match");
+}	
